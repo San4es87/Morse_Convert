25d49b739da931614f975323c41a17b90e7a03f0
San4esVolley <Dmytriiev7@gmail.com>
2021-12-19 17:57:36 +0300

    add css

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import alphabet
 
 
 
@@ -13,57 +14,22 @@ def index():
 
 @app.route('/a', methods=['GET', 'POST'])
 def translate():
-    alphabet_morse = {"A": ".-",
-                      "B": "-...",
-                      "C": "-.-.",
-                      "D": "-..",
-                      "E": ".",
-                      "F": "..-.",
-                      "G": "--.",
-                      "H": "....",
-                      "I": "..",
-                      "J": ".---",
-                      "K": "-.-",
-                      "L": ".-..",
-                      "M": "--",
-                      "N": "-.",
-                      "O": "---",
-                      "P": ".--.",
-                      "Q": "--.-",
-                      "R": ".-.",
-                      "S": "...",
-                      "T": "-",
-                      "U": "..-",
-                      "V": "...-",
-                      "W": ".--",
-                      "X": "-..-",
-                      "Y": "-.--",
-                      "Z": "--..",
-                      "0": "-----",
-                      "1": ".----",
-                      "2": "..---",
-                      "3": "...--",
-                      "4": "....-",
-                      "5": ".....",
-                      "6": "-....",
-                      "7": "--...",
-                      "8": "---..",
-                      "9": "----.",
-                      " ": "  "}
-    q = request.form['text']
+    text = request.form['text']
     # Convertor Morse
     is_on = True
     while is_on:
-        message = q.upper()
         try:
-            new_list = [alphabet_morse[letter] for letter in message]
-            q = ' '.join(new_list)
+            message = text.upper()
+            new_list = [alphabet.encode[letter] for letter in message]
+            text = ' '.join(new_list)
             is_on = False
         except KeyError:
-            print("Sorry please only English")
+            text = "Sorry please only English"
             is_on = False
 
-    return render_template('index.html', q=q)
+
+
+    return render_template('index.html', text=text)
 
 
 
